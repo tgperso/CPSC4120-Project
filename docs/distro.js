dd7bf27c4b677f991b7cee7794b2fae1e61a017b
Thomas Personett <tgperso@clemson.edu>
2023-12-02 21:03:42 -0500

    made good changes on distro graph

--- docs/distro.js
+++ docs/distro.js
@@ -1,6 +1,14 @@
+// Get viewport size
+viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+// Define maximum width
+maxWidth = 1000; // Change this value to your desired maximum width
+
+// Adjust SVG size based on viewport size
 margin = { top: 20, right: 20, bottom: 40, left: 40 };
-width = 1000 - margin.left - margin.right;
-height = 800 - margin.top - margin.bottom;
+width = Math.min(maxWidth, viewportWidth) - margin.left - margin.right;
+height = viewportHeight - margin.top - margin.bottom;
 
 // Create an SVG container for the density graph
 svg = d3
@@ -10,8 +18,32 @@ svg = d3
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Load data from "num1ArtistsByYear.csv"
-d3.csv("num1Artists.csv").then((data) => {
+
+// Load data from "num1ArtistsByYear.csv" asynchronously
+fetch("num1Artists.csv").then(response => response.text()).then(data => {
+  // Parse CSV data
+  data = d3.csvParse(data);
+  
+  var artists = [...new Set(data.map(d => d.artist))]
+
+  var colors = function(){
+    var arr = []
+    for (var i = 0; i < artists.length; i++) {
+      arr.push(d3.interpolateRainbow(i / artists.length))
+    }
+    return arr
+  }()
+
+  let maxPercentage = d3.max(data, d => d.percentage_of_year);
+
+  let opacityScale = d3.scaleLinear()
+    .domain([0, maxPercentage])
+    .range([0, 1]);
+
+  var colorScale = d3.scaleOrdinal()
+    .domain(artists)
+    .range(colors)
+
   // Define x and y scales based on your data
   const xScale = d3
     .scaleLinear()
@@ -40,6 +72,10 @@ d3.csv("num1Artists.csv").then((data) => {
     .attr("in", "SourceGraphic") // Apply the blur to the entire shadow circle
     .attr("stdDeviation", 2); // Adjust the standard deviation for the desired blur amount
 
+  const tooltip = d3.select(".tooltip")
+  const radius = 15;
+  let clicked = false;
+
   // Plot circles for the density graph with blurred stroke
   const circles = svg
     .selectAll("circle")
@@ -48,17 +84,55 @@ d3.csv("num1Artists.csv").then((data) => {
     .append("circle")
     .attr("cx", (d) => xScale(d.year))
     .attr("cy", (d) => yScale(d.percentage_of_year))
-    .attr("r", 20)
-    .style("fill", "hsl(230, 100%, 75%)")
-    .style("opacity", 0.3)
-    .style("stroke", (d) => {if (d.percentage_of_year >= 25) return "black";})
-    .style("stroke-opacity", (d) => {if (d.percentage_of_year >= 25) return 1;})
-    .style("stroke-width", (d) => {if (d.percentage_of_year >= 25) return 3;})
-    .style("mix-blend-mode", "overlay")
+    .attr("r", radius)
+    // .style("stroke", "black")
+    .attr("opacity", d => opacityScale(d.percentage_of_year))
+    .style("fill", d => d3.color(colorScale(d.artist)))
+    .style("overflow", "clip")
+    .on("mouseover", function(event, d) {
+      if (!clicked) {
+      // if (this.cy >= yScale(.25)) { // Check if popularity is greater than 25%
+        // console.log(d)
+        const [mouseX, mouseY] = [event.pageX, event.pageY];
+    
+        tooltip
+          .style("display", "block")
+          .html(`<strong>Artist:</strong> ${d.artist}<br><strong>Year:</strong> ${d.year}<br><strong>Popularity:</strong> ${d.percentage_of_year}%`)
+          .style("left", mouseX + 20 + "px")
+          .style("top", mouseY - 30 + "px");
+        
+        d3.select(this)
+          .attr("r", (d) => radius + 2)
+          .style("transition", "all 0.1s ease")
+          .raise()
+          .style("opacity", 1)
+      }
+    })
+    .on("mouseout", function() {
+      if (!clicked) {
+        tooltip.style("display", "none");
+        d3.select(this)
+          .attr("r", radius)
+          .style("transition", "all 0.1s ease")
+          .style("opacity", d => opacityScale(d.percentage_of_year))
+      }
+    })
+    .on("click", function(event, d) {
+      clicked = !clicked
+      if(clicked) {
+        d3.select(this)
+          .raise()
+          .transition()
+          .duration(100)
+          .style("opacity", 1)
+          .style("opacity", d => opacityScale(d.percentage_of_year))
+      }
+    });
     // .style("filter", "url(#stroke-blur)"); // Apply the Gaussian blur filter to the stroke
 
   // Create a tooltip element to display artist name, percentage, and year
-  const tooltip = d3.select(".tooltip")
+
+
 
   circles.append("circle")
   // .on("mouseover", function(event, d) {
