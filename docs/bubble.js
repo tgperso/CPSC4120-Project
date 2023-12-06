c79cbde52ad0ab03f8d87008fd49565c6af64474
Michael Harris <mah6@g.clemson.edu>
2023-12-05 21:20:37 -0500

    Fixed sizing and added more dots

--- docs/bubble.js
+++ docs/bubble.js
@@ -1,5 +1,5 @@
-var width = 800
-var height = 500
+var width = 700
+var height = 375
 
 
 d3.csv("topArtists.csv").then(
@@ -17,8 +17,6 @@ function updateSVGBubbles(data) {
         .domain(eras)
         .range(colors)
 
-    console.log(data)
-    console.log(data[0]['total_track_pop'])
     var top_tracks = function(d, n) {
         data.sort(function(a, b) {
             return b['total_track_pop'] - a['total_track_pop'];
@@ -26,17 +24,14 @@ function updateSVGBubbles(data) {
         return data.slice(0, n);
     }
 
-    var top_tracks_50 = top_tracks(data, 50);
-    var top_tracks_max = top_tracks_50[0]['total_track_pop']; 
-    var top_tracks_min = top_tracks_50[49]['total_track_pop'];
-    console.log(top_tracks_max);
-    console.log(top_tracks_min);
+    var top_tracks_200 = top_tracks(data, 200);
+    var top_tracks_max = top_tracks_200[0]['total_track_pop']; 
+    var top_tracks_min = top_tracks_200[199]['total_track_pop'];
 
     var radScale = d3.scaleLinear()
         .domain([top_tracks_min, top_tracks_max])
-        .range([6,height/9]);
+        .range([6,height/15]);
 
-    console.log(top_tracks_50);
     var svg = d3.select("#bubble-graph")
         .style("width", width)
         .style("height", height)
@@ -50,13 +45,13 @@ function updateSVGBubbles(data) {
         .style("font-family", "Poppins")
         .style("font-weight", "bold")
         .style("margin-bottom", "30px")
-        .text("Top 50 Tracks by All-Time Popularity")
+        .text("Top 200 Tracks by All-Time Popularity")
 
     const tooltip = d3.select(".tooltip")
 
     var node = svg.append("g")
         .selectAll("circle")
-        .data(top_tracks_50)
+        .data(top_tracks_200)
         .enter()
         .append("circle")
             .attr("cx", width / 2)
@@ -68,13 +63,14 @@ function updateSVGBubbles(data) {
             
                 tooltip
                 .style("display", "block")
-                .html(`<strong>Track:<strong> ${d.title} <br><strong>Artist:</strong> ${d.artist}<br><strong>Popularity:</strong> ${parseInt(d.total_track_pop)}`)
+                .html(`<strong>Track:</strong> ${d.title} <br><strong>Artist:</strong> ${d.artist}<br><strong>Popularity:</strong> ${parseInt(d.total_track_pop)}`)
                 .style("left", mouseX + 20 + "px")
                 .style("top", mouseY - 30 + "px");
                 
                 d3.select(this)
                 .attr("r", d => radScale(d.total_track_pop)+4)
                 .style("transition", "all 0.1s ease")
+                .attr("fill", d => d3.color(colorScale(d.era)).darker(2))
                 .raise()
             })
             .on("mouseout", function(d) {
@@ -83,15 +79,16 @@ function updateSVGBubbles(data) {
                 d3.select(this)
                     .attr("r", d => radScale(d.total_track_pop))
                     .style("transition", "all 0.1s ease")
+                    .attr("fill", d => d3.color(colorScale(d.era)))
             })
 
-    var simulation = d3.forceSimulation(top_tracks_50)
+    var simulation = d3.forceSimulation(top_tracks_200)
 
     simulation
-        .force("center", d3.forceCenter().x(width / 2).y(height / 2 + 20).strength(0.5))
-        .force("charge", d3.forceManyBody().strength(0.5))
+        .force("center", d3.forceCenter().x(width / 2).y(height / 2 + 20).strength(0.9))
+        .force("charge", d3.forceManyBody().strength(0.9))
         .force("collide", d3.forceCollide().strength(0.6)
-        .radius(function(d){ return radScale(d.total_track_pop)+2; })
+        .radius(function(d){ return radScale(d.total_track_pop)+0.5; })
         .iterations(1))
         .on("tick", function(d){
             node
